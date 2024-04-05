cfbd65dda1f67dd0159f910317c60a311a075701
Ilya Stone <tnt228337@gmail.com>
2024-04-05 09:13:53 +0300

    created config parse with cleanenv

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type Config struct {
+	Listen  Listen  `yaml:"listen"`
+	Storage Storage `yaml:"storage"`
+}
+
+type Listen struct {
+	Port string `yaml:"port"`
+}
+
+type Storage struct {
+	Host     string `yaml:"localhost"`
+	Port     string `yaml:"port"`
+	DbName   string `yaml:"database"`
+	UserName string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+var cfg *Config
+
+func MustConfig() *Config {
+	cfg = &Config{}
+
+	if err := cleanenv.ReadConfig("config.yml", cfg); err != nil {
+		panic("cannot read config " + err.Error())
+	}
+
+	return cfg
+}
